8989bb05d1ef686f8ab1f369d163b0e895bdcca4
spacemanlol <shafinschowdhury@gmail.com>
2019-08-05 12:13:02 -0500

    initial build

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Created React Class Based Component - Other is function component
+class App extends React.Component {
+    render() {
+        return(
+            <h1>Youtube Clone Test</h1>
+        )
+    }
+}
+
+// Dummy function based components
+// const App = () => {
+//     return (<h1>Youtube Test</h1>)
+// }
+export default App;
